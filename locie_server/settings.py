426cfc35d445d5e2962303d0425b704f3190e02c
Locie <locieteam2019@gmail.com>
2020-01-02 19:38:03 +0530

    Setting

--- locie_server/settings.py
+++ locie_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%^*2esaf@ou%psce0oc%^tqw@)_55_=f#0wr24-q=ktx^!_zi3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['ec2-13-233-123-86.ap-south-1.compute.amazonaws.com', '13.233.123.86','ec2-13-233-123-86.ap-south-1.compute.amazonaws.com:8000', '13.233.123.86:8000']
+ALLOWED_HOSTS =['ec2-13-233-123-86.ap-south-1.compute.amazonaws.com', '13.233.123.86',]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
